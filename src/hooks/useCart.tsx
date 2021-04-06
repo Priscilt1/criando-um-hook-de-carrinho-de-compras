eaac511e494e01cfd56c262b8194b873855eefcd
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-04-06 17:55:25 -0300

    Page Home

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,3 +1,4 @@
+import { debug } from 'node:console';
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
@@ -23,20 +24,51 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart') //Buscar dados do localStorage 
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    // debugger
 
-    return [];
+    if (storagedCart) {
+      return JSON.parse(storagedCart);//(como a funcao só aceita string ou null, precisa converter e depois reconverter para o array de produtos)
+    }
+
+    return []; //se for null, retorna um array vazio 
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart] //criando um novo array a partir do valor que ja tem no carrinho (mantendo a imutabilidade)
+      const productExists = updatedCart.find(product => product.id === productId) //verificando se o produto exite no carrinho
+  
+      const stock = await api.get(`/stock/${productId}`) //passando a rota do estoque
+      const stockAmount = stock.data.amount //quantidade que tem em estoque 
+      const currentAmount = productExists ? productExists.amount : 0 //quantidade atual do produto no carrinho
+      const amount = currentAmount + 1 //quantidade desejada para acrescentar
+      
+      // verificar se tem a quantidade no estoque
+      if (amount > stockAmount) { // se a quantidade desejada for maior que o estoque...
+        toast.error('Quantidade solicitada fora de estoque');
+        return 
+      }
+
+      if (productExists) {
+        productExists.amount = amount //se o produto ja existir no carrinho so atualiza a quantidade
+      } else {
+        const product = await api.get(`/products/${productId}`)
+
+        const newProduct = {
+          ...product.data,
+          amount: 1 //acrescenta um porque sera a primeira vez que será colocado no carrinho 
+        }
+        updatedCart.push(newProduct)
+      }
+
+      // atualizando o carrinho 
+      setCart(updatedCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)) //JSON.stringify(updatedCart) transformando em string 
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { MdAddShoppingCart } from 'react-icons/md';
+import React, { useState, useEffect } from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-import { ProductList } from './styles';
-import { api } from '../../services/api';
-import { formatPrice } from '../../util/format';
-import { useCart } from '../../hooks/useCart';
+import { ProductList } from "./styles";
+import { api } from "../../services/api";
+import { formatPrice } from "../../util/format";
+import { useCart } from "../../hooks/useCart";
 
 interface Product {
   id: number;
@@ -22,44 +22,64 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  //deve possuir as informações da quantidade de cada produto no carrinho
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = { ...sumAmount };
+    newSumAmount[product.id] = product.amount; //[product.id] é um objeto. É uma forma dinamica de pegar uma chave. Então esta associando o produto com a sua quantidade
+
+    return newSumAmount;
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      const response = await api.get<Product[]>("products"); //pegando os produtos da api
+
+      //formatando o price
+      const data = response.data.map((product) => ({
+        //retornando um objeto
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(data);
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    //adicionar produto. (regras de negocio dentro do hook)
+    addProduct(id);
   }
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+      {/* deixar os dados dinamixos */}
+      {products.map((product) => ( //pegando o formattedProduct 
+        <li key={product.id}> {/* quando dar o map, o primeiro elemento tem que tem uma key*/} 
+          <img
+            src={product.image}
+            alt={product.title}
+          />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {cartItemsAmount[product.id] || 0} 
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 };
